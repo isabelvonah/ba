5b75e876dc252ff5231f7b211970d123aedb6dde
Isabel von Ah <isabelvonah@gmail.com>
2023-04-02 12:19:24 +0200

    add legend

--- playground/assets/script.js
+++ playground/assets/script.js
@@ -101,6 +101,35 @@ const changeColor = (el) => {
     
 }
 
+const addLegendText = (div, text) => {
+    div.innerHTML += text;
+}
+
+const addLegendRow = (seatId, seatClass, text) => {
+
+    let seat = document.createElement("div");
+    seat.setAttribute("id", seatId);
+    seat.classList.add("seat");
+    seat.classList.add(seatClass);
+
+    let row = document.createElement("div");
+    row.classList.add("legendRow");
+    row.appendChild(seat);
+    row.innerHTML += text;
+
+    let legend = document.getElementById("legend");
+    legend.appendChild(row);
+}
+
+const addLegend = () => {
+
+    addLegendRow("legend-category-A", "category-A", "Kategorie A");
+    addLegendRow("legend-category-B", "category-B", "Kategorie B");
+    addLegendRow("legend-taken", "taken", "besetzt");
+    addLegendRow("legend-selected", "selected", "ausgewählt");
+
+}
+
 
   
 const trackMousePosition = () => {
@@ -150,6 +179,7 @@ const untrackMouseClicks = () => {
 
 const startTracking = () => {
     addSeatSetting(setting1);
+    addLegend();
     trackMousePosition();
     trackMouseClicks();
 }
