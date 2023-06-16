382f845cb6e5ce3b0cd15aedcab8b1d6ad2b5ae7
Isabel von Ah <isabelvonah@gmail.com>
2023-06-16 18:05:54 +0200

    add summary overlay

--- project/js/main.js
+++ project/js/main.js
@@ -1,5 +1,4 @@
 const changeColor = (el) => {
-    console.log(el);
 
     if (document.getElementsByClassName("selected").length > 0) {
         document.getElementsByClassName("selected")[0].classList.remove("selected");
@@ -9,4 +8,9 @@ const changeColor = (el) => {
 
 }
 
+const finishTask = () => {
+
+    document.getElementById("summaryOverlay").classList.remove("hidden")
+
+}
 
