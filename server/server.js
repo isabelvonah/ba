6c8de89e481a29bb223940ebe8345815b0e01d1b
Isabel von Ah <isabelvonah@gmail.com>
2023-06-18 17:31:52 +0200

    server setup

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/upload', upload.single('csvFile'), (req, res) => {
+  const file = req.file;
+  const filename = req.file.originalname;
+  console.log(filename)
+
+
+  if (!file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  const newFilename = `${filename}_${Date.now()}.csv`;
+  const destinationPath = path.join(__dirname, 'uploads', newFilename);
+
+  fs.renameSync(file.path, destinationPath);
+
+  res.status(200).send('File uploaded successfully.');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000.');
+});
